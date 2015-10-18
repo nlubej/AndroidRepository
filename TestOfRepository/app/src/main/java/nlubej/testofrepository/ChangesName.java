87ee3b69202e336b5aa427f9c9c2c96b9485668d
nlubej <neejc.lubej@gmail.com>
2015-10-18 18:15:17 +0200

    changedf file name

--- TestOfRepository/app/src/main/java/nlubej/testofrepository/ChangesName.java
+++ TestOfRepository/app/src/main/java/nlubej/testofrepository/ChangesName.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ChangeClassName extends AppCompatActivity {
+public class ChangesName extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
